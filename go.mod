041c9b08b290cb237f0ddc050580cb8a889b2f2b
Alex <alex.v.novoseltsev@gmail.com>
2025-07-15 12:04:13 +0700

    add gitignore, go mode

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AlexeyNovoseltsev/Golang/tree/1-start
+
+go 1.24.3
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
 	const USDToEUR =  0.86
 	const USDToRUB = 78.00
 	const EURToRUB =  USDToRUB / USDToEUR
+	fmt.Print(EURToRUB)
 }
